1356a2ab061be6f54749d8f1a82d91ee7eea63e2
Vaibhav Shukla <63441521+raxvab@users.noreply.github.com>
2022-05-23 03:19:48 +0530

    Update fifth.js

--- src/sections/fifth.js
+++ src/sections/fifth.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import "./third.css"
+import "./fifth.css"
 function Fifth() {
   return (
-    <div className="d1 fifth">
+    <div className="d1 fifth" style={{background:"#0086FB"}}>
 <div className='text'>
   <span className='title'>
   run is now
@@ -15,7 +15,7 @@ Every step you take, every move you make - we will be rewarding you with rare NF
 </span>
 <button className='btn' href="#">Know More</button>
 </div>
-<div className='img'>
+<div className='img-fifth'>
 <img src="fifth.png" />
 </div>
      </div>
