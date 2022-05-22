b72f6f07c55817894f1e2af9d933a7f7c59d8a7e
Vaibhav Shukla <63441521+raxvab@users.noreply.github.com>
2022-05-23 03:51:52 +0530

    Add files via upload

---
+++ src/sections/Navbar.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+import "./Navbar.css"
+function Navbar() {
+  const [linksopen, setlinksopen] = useState(true);
+  return (
+    <div className='navbar' >
+      <div className='menu'>
+      <span>
+        Logo
+      </span>
+      <div className="" onClick={()=>setlinksopen(!linksopen)} >
+        <img src='menu.svg'>
+        </img>
+      </div>
+      </div>
+      <div className={`links ${linksopen?"closed":""}`}>
+        <a href='#'>Home</a>
+        <a href='#'>About</a>
+        <a href='#'>Contact</a>
+
+      </div>
+
+      </div>
+  )
+}
+
+export default Navbar
