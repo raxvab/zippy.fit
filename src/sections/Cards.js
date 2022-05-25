05c0aa7dd8adaf824a225c8e3818a0b0d85e3cc2
codemail-sudhanshu <sudhanshu31001@gmail.com>
2022-05-26 04:36:35 +0530

    changes

---
+++ src/sections/Cards.js
@@ -0,0 +1,103 @@
+import React from 'react'
+import "./Cards.css"
+function Cards() {
+  return (
+    <div className='d1' style={{ color: 'white' }}>
+      <span className='title card-title' >
+        enlist your<br /> running clubs
+      </span>
+      <div className='cards'>
+        <div className='card-child' >
+          <img className='card-img' src='card-one.png'>
+          </img>
+          <div className='card-info'>
+            <span>Bangalore Runners club
+            </span>
+            <span>
+              Bangalore Runners club is a group of self motivated runners aiming to excel in the area of personal fitness
+
+            </span>
+            <div className='profiles'>
+              <div className='card-profile'>
+                <img src='card-one.png'></img>
+              </div>
+              <div className='card-profile'>
+                <img src='card-one.png'></img>
+              </div>  <div className='card-profile'>
+                <img src='card-one.png'></img>
+              </div>  <div className='card-profile'>
+                <img src='card-one.png'></img>
+              </div>
+              {/* <img  src='card-one.png'></img> */}
+
+
+            </div>
+            <button className='btn card'> View Club</button>
+
+          </div>
+
+        </div>
+        <div className='card-child' >
+          <img className='card-img' src='card-one.png'>
+          </img>
+          <div className='card-info'>
+            <span>Bangalore Runners club
+            </span>
+            <span>
+              Bangalore Runners club is a group of self motivated runners aiming to excel in the area of personal fitness
+
+            </span>
+            <div className='profiles'>
+              <div className='card-profile'>
+                <img src='card-one.png'></img>
+              </div>
+              <div className='card-profile'>
+                <img src='card-one.png'></img>
+              </div>  <div className='card-profile'>
+                <img src='card-one.png'></img>
+              </div>  <div className='card-profile'>
+                <img src='card-one.png'></img>
+              </div>
+              {/* <img  src='card-one.png'></img> */}
+
+
+            </div>
+
+          </div>
+
+        </div> <div className='card-child' >
+          <img className='card-img' src='card-one.png'>
+          </img>
+          <div className='card-info'>
+            <span>Bangalore Runners club
+            </span>
+            <span>
+              Bangalore Runners club is a group of self motivated runners aiming to excel in the area of personal fitness
+
+            </span>
+            <div className='profiles'>
+              <div className='card-profile'>
+                <img src='card-one.png'></img>
+              </div>
+              <div className='card-profile'>
+                <img src='card-one.png'></img>
+              </div>  <div className='card-profile'>
+                <img src='card-one.png'></img>
+              </div>  <div className='card-profile'>
+                <img src='card-one.png'></img>
+              </div>
+              {/* <img  src='card-one.png'></img> */}
+
+
+            </div>
+          </div>
+
+        </div>
+
+
+      </div>
+    </div>
+  )
+}
+
+export default Cards
--- src/sections/HeroSection.js
+++ src/sections/HeroSection.js
@@ -6,14 +6,14 @@ function HeroSection() {
     <div className='hero-cont' style={{ backgroundImage: `url(${version?"./demo1.png":"demo4.png"})` }} onClick={()=>setversion(!version)}>
       <span className='hero-title'>
       revolutionizing <br/>
-running for<br/>
-{version?" indoor ":" outdoor "}
- runners.
+      running for<br/>
+     {version?" indoor ":" outdoor "}
+        runners.
       </span>
       <span>
       Zippy is world’s first move to earn metaverse focussed on outdoor and indoor runners, joggers and walkers.
       </span>
-      <button className='btn'> How it works</button>
+      <button className='btn' onClick={()=> console.log("sad") }> How it works</button>
         </div>
   )
 }
